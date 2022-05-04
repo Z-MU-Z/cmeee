1b71d285c1c6fdabeaef7960d27090e273ba69dd
stu915 <stu915@gpu09.pi.sjtu.edu.cn>
2022-05-04 23:26:40 +0800

    crf_nested

--- src/model.py
+++ src/model.py
@@ -250,3 +250,51 @@ class BertForCRFHeadNER(BertPreTrainedModel):
         
         return output
 
+class BertForCRFHeadNestedNER(BertPreTrainedModel):
+    config_class = BertConfig
+    base_model_prefix = "bert"
+
+    def __init__(self, config: BertConfig, num_labels1: int, num_labels2: int):
+        super().__init__(config)
+        self.config = config
+
+        self.bert = BertModel(config)
+        '''NOTE: This is where to modify for Nested NER.
+        '''
+        self.classifier1 = CRFClassifier(config.hidden_size, num_labels1, config.hidden_dropout_prob)
+        self.classifier2 = CRFClassifier(config.hidden_size, num_labels2, config.hidden_dropout_prob)
+        self.init_weights()
+
+    def forward(
+            self,
+            input_ids=None,
+            attention_mask=None,
+            token_type_ids=None,
+            position_ids=None,
+            head_mask=None,
+            inputs_embeds=None,
+            labels=None,
+            labels2=None,
+            output_attentions=None,
+            output_hidden_states=None,
+            return_dict=None,
+            no_decode=False,
+    ):
+        sequence_output = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )[0]
+
+        '''NOTE: This is where to modify for Nested NER.
+        Use the above function _group_ner_outputs for combining results.
+        '''
+        output = self.classifier1.forward(sequence_output, attention_mask, labels, no_decode=no_decode)
+        output2 = self.classifier2.forward(sequence_output, attention_mask, labels2, no_decode=no_decode)
+        return _group_ner_outputs(output, output2)
