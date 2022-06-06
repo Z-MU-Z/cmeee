dc059479faf27ab12ad5a762ed3afd15443f7083
cantabile-kwok <58417810+cantabile-kwok@users.noreply.github.com>
2022-06-06 23:37:51 +0800

    Update vis HTML tool

--- src/eval_from_json.py
+++ src/eval_from_json.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 from ee_data import EE_label2id, EE_id2label, EE_id2label1, EE_id2label2, EE_label2id1, EE_label2id2, LABEL1, LABEL2, \
     LABEL
 from metrics import ComputeMetricsForNestedNER, ComputeMetricsForNER, EvalPrediction
@@ -22,8 +24,8 @@ def f1_from_jsons(gt_label_json, pred_label_json, nested=True):
     for idx, (label_term, pred_term) in enumerate(zip(gt_label_dict, pred_label_dict)):
         assert label_term['text'] == pred_term['text']
         l = len(label_term['text'])
-        # if l > max_len:
-        #     print(label_term['text'])
+        if l > max_len:
+            print(label_term['text'])
         max_len = max(l, max_len)
     print(max_len)
 
@@ -75,53 +77,135 @@ def f1_from_jsons(gt_label_json, pred_label_json, nested=True):
     return metrics['f1']
 
 
-def create_html(gt_label_json, pred_label_json, file_path='../result.html', sample_num=1000):
+def create_html(gt_label_json, pred_label_json, file_path='../result.html', sample_num=1000, nested=True):
     gt_label_dict = json.load(open(gt_label_json))
     pred_label_dict = json.load(open(pred_label_json))
+    itr = random.sample(list(enumerate(zip(gt_label_dict, pred_label_dict))), sample_num)
+
     with open(file_path, 'w') as f:
-        f.write("<!DOCTYPE html>\n"
-                "<html lang=\"en\">\n"
-                "<head>\n"
-                "<meta charset=\"UTF-8\">\n"
-                "<title>CMeEE Prediction</title>\n"
-                "</head>\n"
-                "<body>\n"
-                "<h1>CMeEE prediction</h1>\n"
-                f"Created on {datetime.datetime.now()}\n<br>\nlabel file: <code>{gt_label_json}</code>\n<br>\nprediction file: <code>{pred_label_json}</code>\n"
-                "<h3>Format: Text, Label, Prediction</h3>\n"
-                "<pre>\n")
-        itr = random.sample(list(enumerate(zip(gt_label_dict, pred_label_dict))), sample_num)
-        for idx, (label_term, pred_term) in itr:
-            text_str = "&#9;".join(list(label_term['text']))
-            f.write(text_str + "\n")
-            label_list = ["O" for _ in range(len(label_term['text']))]
-            pred_list = ["O" for _ in range(len(label_term['text']))]
-            for i, ent in enumerate(label_term['entities']):
-                label_list[ent['start_idx']] = f"B-{ent['type']}"
-                label_list[ent['start_idx'] + 1: ent['end_idx'] + 1] = [f"I-{ent['type']}"] * (ent['end_idx'] -ent['start_idx'])
-            for i, ent in enumerate(pred_term['entities']):
-                pred_list[ent['start_idx']] = f"B-{ent['type']}"
-                pred_list[ent['start_idx'] + 1: ent['end_idx'] + 1] = [f"I-{ent['type']}"] * (ent['end_idx'] -ent['start_idx'])
-            for ent in pred_term['entities']:
-                if ent in label_term['entities']:
-                    label_list[ent['start_idx']] = "<span style=\"color: green\">" + label_list[ent['start_idx']]
-                    label_list[ent['end_idx']] = label_list[ent['end_idx']] + "</span>"
-                    pred_list[ent['start_idx']] = "<span style=\"color: green\">" + pred_list[ent['start_idx']]
-                    pred_list[ent['end_idx']] = pred_list[ent['end_idx']] + "</span>"
-                else:
-                    pred_list[ent['start_idx']] = "<span style=\"color: red\">" + pred_list[ent['start_idx']]
-                    pred_list[ent['end_idx']] = pred_list[ent['end_idx']] + "</span>"
-            for ent in label_term['entities']:
-                if ent not in pred_term['entities']:
-                    label_list[ent['start_idx']] = "<span style=\"color: red\">" + label_list[ent['start_idx']]
-                    label_list[ent['end_idx']] = label_list[ent['end_idx']] + "</span>"
-            label_line = "&#9;".join(label_list) + '\n'
-            pred_line = "&#9;".join(pred_list) + '\n'
-            f.write(label_line)
-            f.write(pred_line)
-            # f.write('\n')
-            # f.write("="*500 + '\n')
-            f.write("<HR>\n")
+        if not nested:
+            f.write("<!DOCTYPE html>\n"
+                    "<html lang=\"en\">\n"
+                    "<head>\n"
+                    "<meta charset=\"UTF-8\">\n"
+                    "<title>CMeEE Prediction</title>\n"
+                    "</head>\n"
+                    "<body>\n"
+                    "<h1>CMeEE prediction</h1>\n"
+                    f"Created on {datetime.datetime.now()}\n<br>\nlabel file: <code>{gt_label_json}</code>\n<br>\nprediction file: <code>{pred_label_json}</code>\n<br>\nNested: <code>True</code>\n"
+                    "<h3>Format: Text, Label, Prediction</h3>\n"
+                    "<pre>\n")
+            for idx, (label_term, pred_term) in itr:
+                text_str = "&#9;".join(list(label_term['text']))
+                f.write(text_str + "\n")
+                label_list = ["O" for _ in range(len(label_term['text']))]
+                pred_list = ["O" for _ in range(len(label_term['text']))]
+                for i, ent in enumerate(label_term['entities']):
+                    label_list[ent['start_idx']] = f"B-{ent['type']}"
+                    label_list[ent['start_idx'] + 1: ent['end_idx'] + 1] = [f"I-{ent['type']}"] * (ent['end_idx'] -ent['start_idx'])
+                for i, ent in enumerate(pred_term['entities']):
+                    pred_list[ent['start_idx']] = f"B-{ent['type']}"
+                    pred_list[ent['start_idx'] + 1: ent['end_idx'] + 1] = [f"I-{ent['type']}"] * (ent['end_idx'] -ent['start_idx'])
+                for ent in pred_term['entities']:
+                    if ent in label_term['entities']:
+                        label_list[ent['start_idx']] = "<span style=\"color: green\">" + label_list[ent['start_idx']]
+                        label_list[ent['end_idx']] = label_list[ent['end_idx']] + "</span>"
+                        pred_list[ent['start_idx']] = "<span style=\"color: green\">" + pred_list[ent['start_idx']]
+                        pred_list[ent['end_idx']] = pred_list[ent['end_idx']] + "</span>"
+                    else:
+                        pred_list[ent['start_idx']] = "<span style=\"color: red\">" + pred_list[ent['start_idx']]
+                        pred_list[ent['end_idx']] = pred_list[ent['end_idx']] + "</span>"
+                for ent in label_term['entities']:
+                    if ent not in pred_term['entities']:
+                        label_list[ent['start_idx']] = "<span style=\"color: red\">" + label_list[ent['start_idx']]
+                        label_list[ent['end_idx']] = label_list[ent['end_idx']] + "</span>"
+                label_line = "&#9;".join(label_list) + '\n'
+                label_line = "<span style=\"background-color: #F0F0F0\">" + label_line + "</span>"
+                pred_line = "&#9;".join(pred_list) + '\n'
+                f.write(label_line)
+                f.write(pred_line)
+                # f.write('\n')
+                # f.write("="*500 + '\n')
+                f.write("<HR>\n")
+        else:
+            f.write("<!DOCTYPE html>\n"
+                    "<html lang=\"en\">\n"
+                    "<head>\n"
+                    "<meta charset=\"UTF-8\">\n"
+                    "<title>CMeEE Prediction</title>\n"
+                    "</head>\n"
+                    "<body>\n"
+                    "<h1>CMeEE prediction</h1>\n"
+                    f"Created on {datetime.datetime.now()}\n<br>\nlabel file: <code>{gt_label_json}</code>\n<br>\nprediction file: <code>{pred_label_json}</code>\n<br>\nNested: <code>True</code>\n"
+                    "<h3>Format: Text, Label1, Prediction1, Label2, Prediction2</h3>\n"
+                    "<pre>\n")
+            for idx, (label_term, pred_term) in itr:
+                text_str = "&#9;".join(list(label_term['text']))
+                f.write(text_str + "\n")
+                label1_list = ["O" for _ in range(len(label_term['text']))]
+                pred1_list = ["O" for _ in range(len(label_term['text']))]
+                label2_list = ["O" for _ in range(len(label_term['text']))]
+                pred2_list = ["O" for _ in range(len(label_term['text']))]
+
+                for i, ent in enumerate(label_term['entities']):
+                    if not ent['type'] in LABEL2:
+                        label1_list[ent['start_idx']] = f"B-{ent['type']}"
+                        label1_list[ent['start_idx'] + 1: ent['end_idx'] + 1] = [f"I-{ent['type']}"] * (ent['end_idx'] -ent['start_idx'])
+                    else:
+                        label2_list[ent['start_idx']] = f"B-{ent['type']}"
+                        label2_list[ent['start_idx'] + 1: ent['end_idx'] + 1] = [f"I-{ent['type']}"] * (ent['end_idx'] -ent['start_idx'])
+
+                for i, ent in enumerate(pred_term['entities']):
+                    if not ent['type'] in LABEL2:
+                        pred1_list[ent['start_idx']] = f"B-{ent['type']}"
+                        pred1_list[ent['start_idx'] + 1: ent['end_idx'] + 1] = [f"I-{ent['type']}"] * (ent['end_idx'] -ent['start_idx'])
+                    else:
+                        pred2_list[ent['start_idx']] = f"B-{ent['type']}"
+                        pred2_list[ent['start_idx'] + 1: ent['end_idx'] + 1] = [f"I-{ent['type']}"] * (ent['end_idx'] -ent['start_idx'])
+
+                for ent in pred_term['entities']:
+                    if not ent['type'] in LABEL2:  # pred 中不是LABEL2的那些实体
+                        if ent in label_term['entities']:
+                            label1_list[ent['start_idx']] = "<span style=\"color: green\">" + label1_list[ent['start_idx']]
+                            label1_list[ent['end_idx']] = label1_list[ent['end_idx']] + "</span>"
+                            pred1_list[ent['start_idx']] = "<span style=\"color: green\">" + pred1_list[ent['start_idx']]
+                            pred1_list[ent['end_idx']] = pred1_list[ent['end_idx']] + "</span>"
+                        else:
+                            pred1_list[ent['start_idx']] = "<span style=\"color: red\">" + pred1_list[ent['start_idx']]
+                            pred1_list[ent['end_idx']] = pred1_list[ent['end_idx']] + "</span>"
+                    else:
+                        if ent in label_term['entities']:
+                            label2_list[ent['start_idx']] = "<span style=\"color: green\">" + label2_list[ent['start_idx']]
+                            label2_list[ent['end_idx']] = label2_list[ent['end_idx']] + "</span>"
+                            pred2_list[ent['start_idx']] = "<span style=\"color: green\">" + pred2_list[ent['start_idx']]
+                            pred2_list[ent['end_idx']] = pred2_list[ent['end_idx']] + "</span>"
+                        else:
+                            pred2_list[ent['start_idx']] = "<span style=\"color: red\">" + pred2_list[ent['start_idx']]
+                            pred2_list[ent['end_idx']] = pred2_list[ent['end_idx']] + "</span>"
+
+                for ent in label_term['entities']:
+                    if not ent['type'] in LABEL2:  # pred 中不是LABEL2的那些实体
+                        if ent not in pred_term['entities']:
+                            label1_list[ent['start_idx']] = "<span style=\"color: red\">" + label1_list[ent['start_idx']]
+                            label1_list[ent['end_idx']] = label1_list[ent['end_idx']] + "</span>"
+                    else:
+                        if ent not in pred_term['entities']:
+                            label2_list[ent['start_idx']] = "<span style=\"color: red\">" + label2_list[ent['start_idx']]
+                            label2_list[ent['end_idx']] = label2_list[ent['end_idx']] + "</span>"
+
+                label1_line = "&#9;".join(label1_list) + '\n'
+                label1_line = "<span style=\"background-color: #F0F0F0\">" + label1_line + "</span>"
+                pred1_line = "&#9;".join(pred1_list) + '\n'
+                label2_line = "&#9;".join(label2_list) + '\n'
+                label2_line = "<span style=\"background-color: #F0F0F0\">" + label2_line + "</span>"
+
+                pred2_line = "&#9;".join(pred2_list) + '\n'
+                f.write(label1_line)
+                f.write(pred1_line)
+                f.write(label2_line)
+                f.write(pred2_line)
+                f.write("<HR>\n")
+
         f.write("</pre>\n"
                 "</body>\n"
                 "</html>\n")
@@ -136,4 +220,5 @@ if __name__ == '__main__':
     create_html('../data/CBLUEDatasets/CMeEE/CMeEE_dev.json',
                 '../ckpts/bert_crf_nested_2022/CMeEE_test.json',
                 file_path='result.html',
-                sample_num=100)
+                sample_num=100,
+                nested=True)
